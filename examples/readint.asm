671372ef1005e1c87967b350ec1c9dc5fa5142db
mjbrusso <mjbrusso@gmail.com>
2023-10-24 09:55:09 -0300

    Update readint example

--- examples/readint.asm
+++ examples/readint.asm
@@ -7,18 +7,11 @@ _start:
 	lea		rdi, [prompt]
 	call	printstr	
 	call	readint
-    mov     r14, rax
-  	mov		rdi, rax
-	call 	printint
-    shl     r14, 1      ;  r14 *= 2     
-	lea		rdi, [str2]
-	call	printstr	
-	mov		rdi, r14
+	shl     rax, 1      ;  rax *= 2     
+  	mov		rdi, rax    
 	call 	printint
 	call 	endl	
 	call 	exit0   
-	
-            
+	            
 section		.data
 prompt:		db 'Input a number: ', 0
-str2:		db ' * 2 = ', 0
